13f8b8eb9269811a90935f47ba19569e8d37bdac
Sergey Popov <poserg@gmail.com>
2020-09-30 19:35:14 +0300

    Новый вариант подключения.
    Светодиоды загораются наоборот - при выключении кнопок

--- busyboard.cpp
+++ busyboard.cpp
@@ -1,47 +1,68 @@
 #include <Arduino.h>
 
 // Задаём номера выводов:
-const int leftButtonPin = 36;
-const int rightButtonPin = 35;
-const int upButtonPin = 37;
-const int downButtonPin = 34;
+const int leftButtonPin = 39;
+const int rightButtonPin = 38;
+const int upButtonPin = 40;
+const int downButtonPin = 41;
 
-const int yellowLedPin =  40;
-const int greenLedPin =  41;
-const int blueLedPin =  38;
-const int redLedPin =  39;
+const int yellowButtonPin = 37;
+const int greenButtonPin = 35;
+const int blueButtonPin = 34;
+const int redButtonPin = 36;
+
+const int yellowLedPin =  33;
+const int greenLedPin =  32;
+const int blueLedPin =  31;
+const int redLedPin =  30;
 
 void setup() {
+	Serial.begin(115200);
+
   pinMode(yellowLedPin, OUTPUT);
   pinMode(greenLedPin, OUTPUT);
   pinMode(blueLedPin, OUTPUT);
   pinMode(redLedPin, OUTPUT);
 
-  pinMode(leftButtonPin, INPUT);
-  pinMode(rightButtonPin, INPUT);
-  pinMode(upButtonPin, INPUT);
-  pinMode(downButtonPin, INPUT);
+  pinMode(yellowButtonPin, INPUT_PULLUP);
+  pinMode(greenButtonPin, INPUT_PULLUP);
+  pinMode(blueButtonPin, INPUT_PULLUP);
+  pinMode(redButtonPin, INPUT_PULLUP);
+
+  pinMode(leftButtonPin, INPUT_PULLUP);
+  pinMode(rightButtonPin, INPUT_PULLUP);
+  pinMode(upButtonPin, INPUT_PULLUP);
+  pinMode(downButtonPin, INPUT_PULLUP);
+
+  digitalWrite(yellowLedPin, LOW);
+  digitalWrite(redLedPin, LOW);
+  digitalWrite(greenLedPin, LOW);
+  digitalWrite(blueLedPin, LOW);
 }
 
 void loop() {
-  if (digitalRead(leftButtonPin) == HIGH) {
-    digitalWrite(yellowLedPin, HIGH);
-  } else {
+  if (digitalRead(leftButtonPin) == LOW or digitalRead(yellowButtonPin) == LOW) {
     digitalWrite(yellowLedPin, LOW);
-  }
-  if (digitalRead(rightButtonPin) == HIGH) {
-    digitalWrite(redLedPin, HIGH);
   } else {
+    digitalWrite(yellowLedPin, HIGH);
+  }
+  if (digitalRead(rightButtonPin) == LOW || digitalRead(redButtonPin) == LOW) {
     digitalWrite(redLedPin, LOW);
-  }
-  if (digitalRead(upButtonPin) == HIGH) {
-    digitalWrite(greenLedPin, HIGH);
   } else {
+    digitalWrite(redLedPin, HIGH);
+  }
+  if (digitalRead(upButtonPin) == LOW || digitalRead(greenButtonPin) == LOW) {
+	  if (digitalRead(upButtonPin) == LOW)
+		  Serial.println("upButton is HIG");
+	  if (digitalRead(greenButtonPin) == LOW)
+		  Serial.println("greenButton is HIG");
     digitalWrite(greenLedPin, LOW);
-  }
-  if (digitalRead(downButtonPin) == HIGH) {
-    digitalWrite(blueLedPin, HIGH);
   } else {
+    digitalWrite(greenLedPin, HIGH);
+  }
+  if (digitalRead(downButtonPin) == LOW || digitalRead(blueButtonPin) == LOW) {
     digitalWrite(blueLedPin, LOW);
+  } else {
+    digitalWrite(blueLedPin, HIGH);
   }
 }
